8fe386a644dd862cd76af37a4577b155a3288926
Dominique Netters <d.d.netters@gmail.com>
2017-04-04 09:32:16 +0200

    fixed assignments in seeder, added tags

--- src/Download/DownloadSeeder.php
+++ src/Download/DownloadSeeder.php
@@ -3,6 +3,7 @@
 use Anomaly\Streams\Platform\Assignment\Contract\AssignmentRepositoryInterface;
 use Anomaly\Streams\Platform\Database\Seeder\Seeder;
 use Anomaly\Streams\Platform\Field\Contract\FieldRepositoryInterface;
+use Anomaly\Streams\Platform\Stream\Contract\StreamRepositoryInterface;
 use Anomaly\Streams\Platform\Stream\StreamModel;
 use Wirelab\DownloadsModule\Download\Contract\DownloadRepositoryInterface;
 
@@ -12,20 +13,20 @@ class DownloadSeeder extends Seeder
 	protected $downloads;
 	protected $fields;
 	protected $assignments;
-	protected $stream;
+	protected $streams;
 
 	public function __construct(
 		DownloadRepositoryInterface $downloads,
         AssignmentRepositoryInterface $assignments,
 		FieldRepositoryInterface $fields,
-		StreamModel $streams
+		StreamRepositoryInterface $streams
 	)
 	{
 		$this->namespace   = 'downloads';
 		$this->downloads   = $downloads;
 		$this->fields      = $fields;
 		$this->assignments = $assignments;
-		$this->stream      = $streams->where('slug','=','downloads')->first();
+		$this->streams     = $streams;
 	}
     /**
      * Run the seeder.
@@ -34,24 +35,30 @@ class DownloadSeeder extends Seeder
     {
     	$this->createField('image','Image','file',false,false,['folders' => [1]]);
     	$this->createField('description','Description','textarea',true,true);
+    	$this->createField('tags','Tags','tags');
     }
 
    	public function createField($slug, $name, $type, $translatable = false, $required = false, $config = [])
    	{
-		$field = $this->fields->create([
-			'namespace' => $this->namespace,
-			'slug' => $slug,
-			'type' => 'anomaly.field_type.' . $type,
-			'locked' => 0,
-			'config' => $config,
-			'en' => [
-				'name' => $name
-			]
-		]);
+   		$field = $this->fields->findBySlug($slug);
+   		$stream = $this->streams->findBySlugAndNamespace('downloads','downloads');
+
+   		if ($field == null) {
+			$field = $this->fields->create([
+				'namespace' => $this->namespace,
+				'slug' => $slug,
+				'type' => 'anomaly.field_type.' . $type,
+				'locked' => 0,
+				'config' => $config,
+				'en' => [
+					'name' => $name
+				]
+			]);
+   		}
 
         $this->assignments->create(
             [
-                'stream'       => $this->stream,
+                'stream'       => $stream,
                 'field'    	   => $field,
                 'translatable' => $translatable,
                 'required'     => $required
